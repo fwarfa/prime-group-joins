728ed00c95c0297cfcdeb37ebf695fc502e03c03
Farah Warfa <fwarfa@gmail.com>
2021-08-24 13:54:58 -0500

    completed base mode

--- joins-solutions.sql
+++ joins-solutions.sql
@@ -1,1 +1,62 @@
--- ANSWERS WILL GO HERE
+-- 1. Get all customers and their addresses.
+SELECT * 
+FROM customers
+JOIN addresses
+	ON customers.id=addresses.customer_id;
+    
+-- 2. Get all orders and their line items (orders, quantity and product).
+SELECT * 
+FROM orders
+JOIN line_items
+	ON orders.id = line_items.order_id;
+
+-- 3. Which warehouses have cheetos?
+SELECT warehouse.warehouse
+FROM warehouse
+JOIN warehouse_product
+	ON warehouse.id = warehouse_product.warehouse_id
+JOIN products
+	ON warehouse_product.product_id = products.id
+WHERE products.description = 'cheetos';
+
+-- 4. Which warehouses have diet pepsi?
+SELECT warehouse.warehouse
+FROM warehouse
+JOIN warehouse_product
+	ON warehouse.id = warehouse_product.warehouse_id
+JOIN products
+	ON warehouse_product.product_id = products.id
+WHERE products.description = 'diet pepsi';
+
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT
+	customers.id as customerId,
+	customers.first_name,
+	customers.last_name, 
+	count(orders.id) as numOfOrders
+FROM orders
+JOIN addresses
+	ON orders.address_id = addresses.id
+JOIN customers
+	ON addresses.customer_id = customers.id
+GROUP BY customers.id;
+
+-- 6. How many customers do we have?
+SELECT 
+	count(customers.id) as number_of_customers 
+FROM customers;
+
+-- 7. How many products do we carry?
+SELECT 
+	count(products.id) as number_of_products
+FROM products;
+
+-- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT 
+	products.description,
+	sum(line_items.quantity) as totalQuantity
+FROM products
+JOIN line_items
+	ON products.id = line_items.product_id
+WHERE products.description = 'diet pepsi'
+GROUP BY products.description;
